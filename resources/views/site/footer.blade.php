c9a5759985b01edc0bd10f2bacf2500a004811a0
Miguel Huerta <miguelhuerta@internetbrands.com>
2015-12-24 17:17:45 -0800

    working commit

--- resources/views/site/footer.blade.php
+++ resources/views/site/footer.blade.php
@@ -1,14 +1,6 @@
-<div class='row' style='background: black'>
-    <div class='col-lg-6'>
-        <nav class='navbar'>
-            <ul class='nav nav-stacked'>
-                <li class='text-center hidden-xs'><a href='#'>About</a></li>
-                <li class='text-center hidden-xs'><a href='#'>Projects</a></li>
-                <li class='text-center hidden-xs'><a href='#'>Downloads</a></li>
-            </ul>
-        </nav>
-    </div>
-    <div class='col-lg-6'>
-        
+<div class='container footer'>
+    <div class='row'>
+        <span>&#64; {{ date('Y') }} Miguel Huerta. All Rights Reserved.
+            This site was created using Laravel 5</span>
     </div>
 </div>
