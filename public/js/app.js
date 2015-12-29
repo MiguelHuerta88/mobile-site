fa4a19e6f83f7bf0b5f0023c170ed717fa92b9da
Miguel Huerta <miguelhuerta@internetbrands.com>
2015-12-28 19:00:08 -0800

    Finishing design touches. Next step is to begin making design changes were they are needed.

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,15 @@ $(document).ready(function(){
     var body =  $("#body").val();
     var title = $("input[name='title']").val();
     var type = $("input[name='type']").val();
-    console.log(title);
     var modal = $(this);
     modal.find('#page-title').text('Title: ' + title);
     modal.find('#page-type').text("Type: " + type);
     modal.find('#content-body').html('Body: ' + nl2br(body));
     });
     
-    function nl2br(el)
-    {
-        var lines = el.split(/\n/);
+    function nl2br(content)
+    {   
+        var lines = content.split(/\n/);
         var text = '';
         for (var i = 0 ; i < lines.length ; i++) {
            text += lines[i] + '<br>';
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,9 @@
 @section('title', 'Welcome to Miguelhuerta.co.nf')
 @section('content')
 <div class='container'>
-    
+    <div class="home-img">
+    </div>
+    <span class="img-title">Developer, problem solver, enthusiast...</span>
+    <span class="img-by">Miguel Huerta</span>
 </div>
 @endsection
