23432852d49d37d6bbad929c513716009b811300
Miguel Huerta <miguelhuerta@internetbrands.com>
2015-12-29 19:19:34 -0800

    some template changes. Added favicon to site.

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.admin')
+@extends('layouts.master')
 @section('title','Login')
 @section('content')
 <div class='container'>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,7 @@
         </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <link rel='stylesheet' type="text/css" href="{{ asset('css/app.css') }}">
+        <link rel="icon" type="image/png" href="{{ asset('images/favicon-16x16.png') }}" sizes="16x16">
        
         <script src="//code.jquery.com/jquery-latest.js"></script>
         <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
