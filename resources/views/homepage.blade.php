726bbc94355fe67e050d663e9b592f0491a46d1f
Miguel Huerta <miguelhuerta@internetbrands.com>
2016-01-01 19:29:30 -0800

    updates.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,7 @@
 <div class='container'>
     <div class="home-img">
     </div>
-    <span class="img-title">Developer, problem solver, enthusiast...</span>
+    {{--<span class="img-title">Developer, problem solver, enthusiast...</span>--}}
     <span class="img-by">Miguel Huerta</span>
 </div>
 @endsection
