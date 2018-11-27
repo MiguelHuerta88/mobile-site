8ffe27a735d7a44f5c864dbadcd9960a1630688f
Miguel Huerta <miguelhuerta@Miguels-MacBook-Pro.local>
2018-11-26 16:31:09 -0800

    Readjusted commadn to run at 3:15 since crons can only run evefy 15 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('wordnik:word')
-            ->dailyAt('03:05')
+            ->dailyAt('03:15')
             ->appendOutputTo(storage_path('/logs/wordnik.log'));
     }
 
